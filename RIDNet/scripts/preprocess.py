7acab92e58d948bc581fde553e3e17a03918a8c3
Alejandro F <afuste7@gmail.com>
2024-11-26 12:00:37 -0500

    add import statements, comment, and initial function code to preprocess file

---
+++ RIDNet/scripts/preprocess.py
@@ -0,0 +1,12 @@
+import cv2
+import os
+import numpy as np
+from glob import glob
+
+'''
+function contains code to convert image to grayscale, resize to 256x256, and normalize pixel values
+'''
+
+
+def preprocess_images(input_folder, output_folder, image_size=(256, 256)):
+    pass
