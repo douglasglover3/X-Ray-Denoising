be3d451459a7800e7d4808a7a31ca4bb5c8c0ea1
Alejandro F <afuste7@gmail.com>
2024-11-26 09:31:48 -0500

    add code to download_dataset function in access_data file

--- RIDNet/scripts/access_data.py
+++ RIDNet/scripts/access_data.py
@@ -1,3 +1,22 @@
 from google.cloud import storage
 import os
 
+
+def download_dataset(bucket_name, source_folder, destination_folder):
+    # Initialize the client
+    client = storage.Client()
+
+    # Specify your bucket name
+    # bucket = client.bucket(bucket_name)
+    bucket = client.get_bucket(bucket_name)
+
+    # List all files in the bucket
+    blobs = bucket.list_blobs(prefix=source_folder)
+
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder)
+
+    for blob in blobs:
+        file_path = os.path.join(destination_folder, os.path.basename(blob.name))
+        blob.download_to_filename(file_path)
+        print(f"Downloaded {blob.name} to {file_path}")
