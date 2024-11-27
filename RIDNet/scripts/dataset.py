396b1afc3aa35ad42473093b7049406dab9256e5
Alejandro F <afuste7@gmail.com>
2024-11-27 11:21:19 -0500

    add getitem dunder method to XRayDataset class

--- RIDNet/scripts/dataset.py
+++ RIDNet/scripts/dataset.py
@@ -36,4 +36,25 @@ class XRayDataset(Dataset):
         """Return the number of paired clean and noisy images."""
         return min(len(self.clean_files), len(self.noisy_files))
 
+    def __getitem__(self, idx):
+        """
+        Get a pair of noisy and clean images.
+
+        Args:
+            idx (int): Index of the image to fetch.
+
+        Returns:
+            Tuple[Tensor, Tensor]: Transformed noisy and clean images.
+        """
+        # Retrieve file paths for clean and noisy images
+        clean_path = self.clean_files[idx]
+        noisy_path = self.noisy_files[idx]
+
+        # Load images
+        clean_img = Image.open(clean_path)
+        noisy_img = Image.open(noisy_path)
+
+        # Transform images and return the pair
+        return self.transform(noisy_img), self.transform(clean_img)
+
 
