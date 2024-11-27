32d2bc1c11520e6b8c6e77d41236e846e22950b4
Alejandro F <afuste7@gmail.com>
2024-11-26 19:47:11 -0500

    add import statements, comment, and initial function code to add_noise file

--- RIDNet/scripts/add_noise.py
+++ RIDNet/scripts/add_noise.py
@@ -1,7 +1,25 @@
 import random
+import numpy as np
+import os
+from glob import glob
+import cv2
 
 """
 
-The dataset is clean so artificial add noise will be added to simulate real-world scenarios
+The dataset is clean so artificial noise will be added to simulate real-world scenarios
+
 
 """
+
+# Function to add different types of noise to an image
+def add_noise(image, noise_type="gaussian"):
+    pass
+
+
+def apply_noise(input_folder, output_folder, noise_type="gaussian"):
+    pass
+
+
+
+
+
