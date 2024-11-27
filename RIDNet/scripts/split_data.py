53624bc73df1866071049072b596a80bc4b83a3b
Alejandro F <afuste7@gmail.com>
2024-11-26 20:12:30 -0500

    add import statements, comment, and initial function code to split_data file

---
+++ RIDNet/scripts/split_data.py
@@ -0,0 +1,8 @@
+from sklearn.model_selection import train_test_split
+import shutil
+
+
+def split_dataset(input_folder, output_base_folder, train_size=0.7, val_size=0.2, test_size=0.1):
+    pass
+
+
