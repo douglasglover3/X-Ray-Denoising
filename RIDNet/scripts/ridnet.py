404b348e8dfb8bcf34aadb607150cd7858563d87
Alejandro F <afuste7@gmail.com>
2024-11-27 09:57:28 -0500

    add class properties for head, body, and tail

--- RIDNet/scripts/ridnet.py
+++ RIDNet/scripts/ridnet.py
@@ -6,4 +6,8 @@ class RIDNet(nn.Module):
     def __init__(self):
         super(RIDNet, self).__init__()
 
-        
+        self.head = nn.Conv2d(1, 64, kernel_size=3, padding=1, bias=True)
+        self.body = nn.Sequential(
+            *[self.residual_dense_block(64) for _ in range(3)]
+        )
+        self.tail = nn.Conv2d(64, 1, kernel_size=3, padding=1, bias=True)
